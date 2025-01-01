666df92e628ee026982fc3885ff15b25512b0e83
dagijosi <dagijossy18@gmail.com>
2025-01-01 16:08:22 +0300

    Enhance Navbars component: Add "Home" navigation item for improved user navigation and accessibility.

--- src/components/Navbars.tsx
+++ src/components/Navbars.tsx
@@ -13,6 +13,7 @@ import {
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 const navItems = [
+  { label: "Home", href: "#home" },
   { label: "About", href: "#about" },
   { label: "Projects", href: "#projects" },
   { label: "Skills", href: "#skills" },
